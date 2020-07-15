a01dafdee6d47affb10f62460992702ff58b2e80
Faheemah Patel <fapatel@c5r1s8.wethinkcode.co.za>
2020-07-15 15:20:39 +0200

    done

--- ex01/ft_recursive_factorial.c
+++ ex01/ft_recursive_factorial.c
@@ -1,32 +1,23 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_interative_factorial.c                          :+:      :+:    :+:   */
+/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fapatel <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2020/07/14 10:14:46 by fapatel           #+#    #+#             */
-/*   Updated: 2020/07/15 15:17:38 by fapatel          ###   ########.fr       */
+/*   Created: 2020/07/15 14:20:59 by fapatel           #+#    #+#             */
+/*   Updated: 2020/07/15 15:17:24 by fapatel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
 #include<stdio.h>
 #include<unistd.h>
 
-int		ft_iterative_factorial(int nb)
-
+int		ft_recursive_factorial(int nb)
 {
-	int		factorial;
-	int		i;
-
 	if (nb < 0)
-		return(0);
-	factorial = 1;
-	i = 2;
-
-	while (i <= nb)
-	{
-	  factorial *= i;
-	  i++;
-	}
-	return (factorial);
+		return (0);
+	if (nb == 0 || nb == 1)
+		return(1);
+	return nb * ft_recursive_factorial(nb - 1);
 }
